241b4f9992b5ce8371d4a8887aee449b2f8d3310
iMaary <m.rodriguesnave@gmail.com>
2019-08-22 11:25:41 -0300

    movimentação horizontal implementada

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -4,7 +4,41 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
+    private float horizontalInput, speed = 5f, forceJump = 5f;
+    private SpriteRenderer sr;
+    private Rigidbody2D rb;
+    private Animator anim;
+    void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
     void FixedUpdate(){
-        
+        Move();
+
+    }
+
+
+    private void Move()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        if (horizontalInput > 0)
+        {
+            transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
+            sr.flipX = false;
+            anim.SetBool("walk", true);
+        }
+        else if (horizontalInput < 0)
+        {
+            transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
+            sr.flipX = true;
+            anim.SetBool("walk", true);
+        }
+        else
+        {
+            anim.SetBool("walk", false);
+        }
     }
 }
