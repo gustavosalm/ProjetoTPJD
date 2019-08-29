f5b7bc087101a6a3a3ec46773b18728f663b3611
iMaary <m.rodriguesnave@gmail.com>
2019-08-29 11:57:22 -0300

    velocidade na animação implementada

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-    private float horizontalInput, verticalInput, speed = 5f, forceJump = 10f;
+    private float horizontalInput, verticalInput, speed = 3f, forceJump = 15f;
     private bool noChao;
 
     private SpriteRenderer sr;
     private Rigidbody2D rb;
     private Animator anim;
+    [SerializeField] private Joystick joystick;
     void Awake()
     {
         sr = GetComponent<SpriteRenderer>();
@@ -25,8 +26,8 @@ public class PlayerBehaviour : MonoBehaviour
     private void Jump()
     {
         noChao = Physics2D.OverlapCircle(transform.position - new Vector3(0, 0.6f), 0.5f, LayerMask.GetMask("Chao"));
-        verticalInput = Input.GetAxisRaw("Vertical");
-        if (noChao && verticalInput > 0)
+        verticalInput = joystick.Vertical;
+        if (noChao && verticalInput >= 0.5f)
         {
             anim.SetBool("jump", true);
             rb.velocity = new Vector2(0, forceJump);
@@ -40,17 +41,19 @@ public class PlayerBehaviour : MonoBehaviour
 
     private void Move()
     {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
+        horizontalInput = joystick.Horizontal * speed;
+
+        anim.speed = (Mathf.Abs(horizontalInput) >= 0.2f)? (Mathf.Abs(horizontalInput) >= 0.8f)? 0.8f : Mathf.Abs(horizontalInput) : (anim.GetBool("walk") == false)? 1f : 0.2f ;
 
         if (horizontalInput > 0)
         {
-            transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
+            transform.Translate(Vector3.right * horizontalInput * Time.deltaTime);
             sr.flipX = false;
             anim.SetBool("walk", true);
         }
         else if (horizontalInput < 0)
         {
-            transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
+            transform.Translate(Vector3.right * horizontalInput * Time.deltaTime);
             sr.flipX = true;
             anim.SetBool("walk", true);
         }
@@ -58,5 +61,8 @@ public class PlayerBehaviour : MonoBehaviour
         {
             anim.SetBool("walk", false);
         }
+
+        horizontalInput = (joystick.Horizontal >= 0.2f) ? speed : (joystick.Horizontal <= -0.2f) ? -speed : 0f;
+        verticalInput = (joystick.Horizontal >= 0.2f) ? speed : (joystick.Horizontal <= -0.2f) ? -speed : 0f;
     }
 }
